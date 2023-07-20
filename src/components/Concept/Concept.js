68a1397f60950a6eb860db78a79ff634a4b935dd
Grady Ku <32969781+gkupdx@users.noreply.github.com>
2023-07-19 19:06:24 -0700

    Initial commit

---
+++ src/components/Concept/Concept.js
@@ -0,0 +1,13 @@
+// Concept component
+
+function Concept(props) {
+  return (
+    <li className="concept">
+      <img src={props.image} alt={props.title} />
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
+export default Concept;
